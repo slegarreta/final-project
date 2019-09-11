082d7c3825d0709ca8f3de7a98715f3d1ed03b4e
Sam Legarreta <samlegarreta@gmail.com>
2019-09-12 01:08:24 +0200

    added heroku app

--- src/Card.js
+++ src/Card.js
@@ -24,9 +24,10 @@ render(){
          <img src="https://www.cartrip.co.za/blog/wp-content/uploads/2017/01/road-trip-travel_500x300_71461060508.jpg"/>
          <p className="row justify-content-center location">{this.props.place.item.city}, {this.props.place.item.country}</p>
          <p>{this.props.place.item.notes}</p>
+
          <input type="text" value={this.state.value} onChange={this.handleUpdateChange} />
         <button onClick={(e) => this.triggerUpdateChange(e)}>
-             Update Notes
+             Update Note
         </button>
          <button className="btn btn-danger" size="sm" onClick={(e) => this.props.delete(this.props.place.key)}>Remove</button>
      </div>
